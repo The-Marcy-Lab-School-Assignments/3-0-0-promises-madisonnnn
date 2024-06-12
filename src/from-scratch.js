fec60fe0df6e8baed4fdb99103e0b10395121772
Marcy <madisontolentino@gmail.com>
2024-06-12 14:54:41 -0400

    done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,40 @@
-const resolvedWrapper = () => {
+const resolvedWrapper = (val) => {
+  const promise = new Promise((resolve,rejected) => {
+    // setTimeout(() => {
+      resolve(val)
+    // }, 500);
+  })
+  return promise 
 };
 
-const rejectedWrapper = () => {
+const rejectedWrapper = (val) => {
+  const promise = new Promise((resolve,rejected) => {
+    //setTimeout(() => {
+      rejected(new Error(val))
+    //}, 500);
+  })
+  return promise 
 };
 
-const handleResolvedPromise = () => {
+const handleResolvedPromise = (promise) => {
+  return promise.then((randomValue) => {
+    console.log(`${randomValue}`)
+    return randomValue
+  })
 };
 
-const handleResolvedOrRejectedPromise = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+  return promise.then((randomValue) => {
+    console.log(`${randomValue}`)
+    return randomValue
+  }).catch((err) => {
+    console.error(`Your error message was: ${err.message}`)
+    return null
+  })
 };
 
-const pauseForMs = () => {
+const pauseForMs = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 module.exports = {
